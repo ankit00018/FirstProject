5be6e0caf2a71a1535d48014be6cea220ba0b76f
ankit00018 <ankitkachhad9@gmail.com>
2025-02-17 10:00:49 +0530

    Postman, routes

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/asyncHandler";
+ 
+const registerUser = asyncHandler ( async (res,req) => {
+    res.status(200).json({
+        message: "Ok"
+    })
+})
+
+
+export {registerUser}
